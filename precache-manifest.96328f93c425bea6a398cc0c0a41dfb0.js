fe55b47e9bcd50609d8ec07bfc4e40bd1208643e
Kaya <serhat.kayaa3@gmail.com>
2020-02-17 01:18:50 +0300

    Updates

---
+++ precache-manifest.96328f93c425bea6a398cc0c0a41dfb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0624f8a9ae7ecae8146869312c2c5ee9",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "486d82df2d1903db4b19",
+    "url": "/weather-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "486d82df2d1903db4b19",
+    "url": "/weather-app/static/js/2.0789df93.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/weather-app/static/js/2.0789df93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1aeb7186d654da38311",
+    "url": "/weather-app/static/js/main.09614541.chunk.js"
+  },
+  {
+    "revision": "48f3787e04d45de6cbd1",
+    "url": "/weather-app/static/js/runtime-main.36d91d67.js"
+  }
+]);
